a89a231f60d2cc1ed5b8ea950874df7f2c6f3e3c
Linx <olga-reshetova@mail.ru>
2024-07-28 10:58:16 +0300

    add footer 2x 3x bg. add blur old

--- gulpfile.js
+++ gulpfile.js
@@ -81,10 +81,10 @@ export const optimizeImages = () => {
 
 export const createWebp = () => {
   return gulp.src([
-    `assets/img/price-bg/*.png`,
+    `assets/img/footer-bg/*.png`,
   ])
     .pipe(webp({quality: 100}))
-    .pipe(gulp.dest(`assets/img/price-bg`))
+    .pipe(gulp.dest(`assets/img/footer-bg`))
 }
 
 // Copy
